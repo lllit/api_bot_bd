11b589b97b28630f1d0e735e043e3e84d93cbfe4
Matias <lit.io30303@gmail.com>
2025-01-14 17:45:49 -0300

    manejando errores

--- automatizacion/gitupdate.py
+++ automatizacion/gitupdate.py
@@ -1,11 +1,15 @@
 import git
 import os
 
-repo_dir = 'D:/LLLIT/Code-W11/PY/api_bot_bd'
-file_path = os.path.join(repo_dir, 'temp_calendar/reservations_all.csv')
+try:
+    repo_dir = 'D:/LLLIT/Code-W11/PY/api_bot_bd'
+    file_path = os.path.join(repo_dir, 'temp_calendar/reservations_all.csv')
 
-repo = git.Repo(repo_dir)
-repo.git.add(file_path)
-repo.index.commit('Actualizar archivo de reservas')
-origin = repo.remote(name='origin')
-origin.push()
+    repo = git.Repo(repo_dir)
+    repo.git.add(file_path)
+    repo.index.commit('Actualizar archivo de reservas')
+    origin = repo.remote(name='origin')
+    origin.push()
+    print("Subido con exito!")
+except Exception as e:
+    print(f"Error al subir archivo a repositorio: {e}")
